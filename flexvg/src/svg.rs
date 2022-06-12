9143bbe5bd6f92925a8b1873d9bdf48ba55b5515
Justin Shrake <justinshrake@gmail.com>
2022-06-12 14:12:09 -0700

    Build svg string with write macro

--- flexvg/src/svg.rs
+++ flexvg/src/svg.rs
@@ -1,25 +1,17 @@
 use crate::prelude::*;
+use std::fmt::Write;
 use std::{fs::File, io::Read, path::Path};
-
-#[inline]
-fn push_line(s: &mut String, line: &str) {
-    *s += line;
-    *s += "\n";
-}
 
 pub(crate) fn compute_svg_string(
     node: FlexGeomNode,
     base_path: Option<&Path>,
 ) -> Result<String, Error> {
-    let mut svg = String::new();
-    let header = format!(
-        r#"<svg version="1.0" width="{}" height="{}" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink">
-        "#,
+    let mut svg = String::with_capacity(8096);
+    writeln!(
+        svg,
+        r#"<svg version="1.0" width="{}" height="{}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">"#,
         node.width, node.height
-    );
-    let footer = r#"</svg>"#;
-    push_line(&mut svg, &header);
+    ).unwrap();
     if let Some(root) = &node.root {
         if let Some(stylesheet) = &root.stylesheet {
             let stylesheet_path = if let Some(base_path) = base_path {
@@ -27,20 +19,19 @@ pub(crate) fn compute_svg_string(
             } else {
                 Path::new(stylesheet).to_path_buf()
             };
-            let contents = if stylesheet_path.exists() {
+            writeln!(svg, "<style>").unwrap();
+            if stylesheet_path.exists() {
                 let mut contents = String::new();
                 File::open(stylesheet_path)?.read_to_string(&mut contents)?;
-                contents
+                writeln!(svg, "{contents}").unwrap();
             } else {
-                stylesheet.clone()
+                writeln!(svg, "{stylesheet}").unwrap();
             };
-            push_line(&mut svg, "<style>");
-            push_line(&mut svg, &contents);
-            push_line(&mut svg, "</style>");
+            writeln!(svg, "</style>").unwrap();
         }
     }
     compute_svg_string_recursive(node, &mut svg, 0.0, 0.0)?;
-    push_line(&mut svg, footer);
+    writeln!(svg, r#"</svg>"#).unwrap();
     Ok(svg)
 }
 
@@ -52,10 +43,10 @@ fn compute_svg_string_recursive(
 ) -> Result<(), Error> {
     let has_elements = node.nodes.iter().any(|n| n.element.is_some());
     if has_elements {
-        push_line(svg, "<g>");
+        writeln!(svg, "<g>").unwrap();
     }
     if let Some(element) = node.element {
-        let element = match element {
+        match element {
             FlexElement::Text(el) => {
                 let x = x + 0.5 * node.width + node.position.x;
                 let y = y + 0.5 * node.height + node.position.y;
@@ -63,9 +54,9 @@ fn compute_svg_string_recursive(
                 let class = el.global.class;
                 let style = el.global.style;
                 let value = el.text;
-                format!(
-                    r#"<text id="{id}" class="{class}" x="{x}" y="{y}" style="{style}">{value}</text>"#
-                )
+                writeln!(
+                    svg,
+                    r#"<text id="{id}" class="{class}" x="{x}" y="{y}" style="{style}">{value}</text>"#).unwrap();
             }
             FlexElement::Rect(el) => {
                 let x = x + node.position.x;
@@ -75,12 +66,10 @@ fn compute_svg_string_recursive(
                 let id = el.global.id;
                 let class = el.global.class;
                 let style = el.global.style;
-                format!(
-                    r#"<rect id="{id}" class="{class}" x="{x}" y="{y}" width="{w}" height="{h}" style="{style}"></rect>"#
-                )
+                writeln!(svg,
+                    r#"<rect id="{id}" class="{class}" x="{x}" y="{y}" width="{w}" height="{h}" style="{style}"></rect>"#).unwrap();
             }
-        };
-        push_line(svg, &element);
+        }
     }
     let mut children = node.nodes;
     // @NOTE(jshrake): Reverse the children so that the
@@ -95,7 +84,7 @@ fn compute_svg_string_recursive(
         )?;
     }
     if has_elements {
-        push_line(svg, "</g>");
+        writeln!(svg, "</g>").unwrap();
     }
     Ok(())
 }
